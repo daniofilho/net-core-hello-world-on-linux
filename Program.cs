7071456c3f925f82f9cba092457e1a39c57592f6
Dânio Filho <danio.filho@navegarti.com.br>
2021-06-03 09:49:11 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace net_core_hello_world_on_linux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+ref: https://jurtzmarcel.medium.com/getting-started-with-net-core-on-arch-linux-5e7b75acda04
+
+run with `dotnet run`
